f73da606ccc62f296e5334cd8caab56a62d8e339
Infinity <n.dima.1998@mail.ru>
2023-03-07 23:24:10 +0300

    Добавил bat файлы для установки python проектов.

---
+++ extractor.bat
@@ -0,0 +1,5 @@
+cd Python\Extractor
+python -m venv venv
+call Python\Extractor\venv\Scripts\activate
+pip install -r "requirements.txt"
+auto-py-to-exe
---
+++ translator.bat
@@ -0,0 +1,5 @@
+cd Python\Translator
+python -m venv venv
+call Python\Translator\venv\Scripts\activate
+pip install -r "requirements.txt"
+auto-py-to-exe
